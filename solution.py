560613091fb0766d1694639b543c9f782bc7e6b4
d1scob4ll <130067358+d1scob4ll@users.noreply.github.com>
2023-04-07 22:12:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,7 +14,7 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     n = len(x)
     s = np.sum(x)
-    x_mean = s / (n * 86**2)
+    x_mean = s / n
     left = (-np.log(alpha/2 + 1/2) - x_mean)/(86**2)
     right = (-np.log(3/2 - alpha/2) - x_mean)/(86**2)
     return left, \
